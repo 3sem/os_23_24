c542ade929b9eccc7160644190206484793f9760
i-am-tost <sidorchuk.me@phystech.edu>
2023-09-26 19:09:00 +0300

    as a result of extensive debugging (putting a bunch of random printfs in random places) it was found out that random file may (and near 100% will) contain a -1 (also known as EOF) in the fucking middle of the file. And because my program uses EOF as a mean of signalling that reading is over parent process breaks from cycle and also stops child by doing so. But it's cool ok yeah.

--- task_2/duplex.cpp
+++ task_2/duplex.cpp
@@ -10,6 +10,8 @@ int main () {
     assert (input != NULL);
     assert (output != NULL);
 
+    size_t counter = 0;
+
     for (int cap = 128; cap <= 1024; cap*=2) {
 
         Duplex lol (cap);
@@ -33,6 +35,9 @@ int main () {
                 printf ("Child recieved %d\n", lol.size);
 
                 fwrite (lol.buf, sizeof (char), charCnt, output);
+
+                counter += cap;
+                printf ("wrote %lu bytes by now\n", counter);
             }
 
             lol.buf[0] = EOF;
@@ -49,13 +54,21 @@ int main () {
 
             printf ("Parent recieved %lu\n", bytesRecieved);
 
+            printf ("buf: [ %d", lol.buf[0]);
+            for (int i = 1; i < cap; i++) printf (", %d", lol.buf[i]);
+            printf ("]\n");
+
             if (lol.buf[0] == EOF) break;
 
             lol.send (0);
             printf ("Parent sent\n");
         }
 
+        waitpid (pid, NULL, 0);
+
         testTransmissionIntegrity (&input, &output, &result, &lol);
+
+        lol.DTOR ();
     }
 
     fclose (input);
