74f1e380ee57f9a9f9bc6db564c5831070063961
Илья Усов <usov.is@phystech.edu>
2023-11-08 16:24:20 +0300

    task4 with setcpu and new graph

--- task_4/b.c
+++ task_4/b.c
@@ -1,10 +1,13 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <pthread.h>
 #include <math.h>
+#include <sched.h> 
+#include <unistd.h>
 
-#define N_POINTS 10000 // Общее количество точек для генерации
+#define N_POINTS 100000 // Общее количество точек для генерации
 #define N_THREADS 1// Число потоков
 
 double foo(double x) {
@@ -29,12 +32,22 @@ void *monte_carlo(void *param) {
 
 int main() {
     pthread_t threads[N_THREADS];
+    cpu_set_t cpuset;
+    pthread_attr_t attr;
+
     double counts[N_THREADS];
 
     srand(time(NULL));
 
+
     for (int i = 0; i < N_THREADS; i++) {
-        pthread_create(&threads[i], NULL, monte_carlo, (void *)&counts[i]);
+        CPU_ZERO(&cpuset);
+        CPU_SET((i%8), &cpuset);
+        int result = pthread_attr_setaffinity_np(&attr, sizeof(cpuset), &cpuset); 
+        if (result != 0) { 
+            perror("Error setting thread cpu core");
+        } 
+        pthread_create(&threads[i], &attr, monte_carlo, (void *)&counts[i]);
     }
 
     for (int i = 0; i < N_THREADS; i++) {
@@ -48,7 +61,7 @@ int main() {
 
     double integral = total_count / N_POINTS;
 
-    //printf("Значение интеграла: %f\n", integral);
+    // printf("Значение интеграла: %f\n", integral);
 
     return 0;
 }
--- task_4/graf.py
+++ task_4/graf.py
@@ -4,17 +4,18 @@ import matplotlib.pyplot as plt
 
 tm = []
 
-for i in range(1, 25):
+for i in range(1, 50):
+    print(i);
     tmp = []
     for j in range(20):
-        os.system(f"gcc a.c -DN_THREADS=i -w")
+        os.system(f"gcc b.c -DN_THREADS={i} -w -Ipthread.h")
         tic = datetime.datetime.now().timestamp()
         os.system(f"./a.out")
         tac = datetime.datetime.now().timestamp()
         tmp.append(tac - tic)
     tm.append(sum(tmp)/len(tmp))
 
-x = [i for i in range(1, 25)]
+x = [i for i in range(1, len(tm)+1)]
 plt.plot(x, tm, label = "зависимость времени исполнения от количества потоков");
 plt.show()
 os.system("rm a.out")
